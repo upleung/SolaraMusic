9e3a067667a96ea267f8b9f0775a7c0050431cc3
upleung <georglee9830@gmail.com>
2026-07-26 23:59:14 +0800

    Add files via upload

--- js/i18n.js
+++ js/i18n.js
@@ -5,18 +5,12 @@
         "酷我音乐": "Kuwo Music",
         "JOOX音乐": "JOOX Music",
         "哔哩哔哩": "Bilibili",
-        "QQ音乐": "Tencent Music",
-        "Apple Music": "Apple Music",
-        "YouTube Music": "YouTube Music",
-        "Spotify": "Spotify"
-        "Tidal": "Tidal",
-        "Qobuz": "Qobuz",
         "极高音质": "High Quality",
         "标准音质": "Standard Quality",
         "高品音质": "High Quality",
         "无损音质": "Lossless",
         "歌词将在此处同步显示": "Lyrics will be displayed here synchronously",
-        "🎧": "🎧",
+        "Made by Wet Dream Boy，免费API来自GD音乐台(music.gdstudio.xyz)，仅供学习交流使用，请支持正版音乐奥！": "Made by Wet Dream Boy. Free API from GD Music (music.gdstudio.xyz). For educational purposes only, please support official releases!",
 
         // index.html
         "探索雷达": "Explore Radar",
@@ -117,14 +111,14 @@
                         }
                     }
                 });
-
+                
                 if (node.tagName === 'TITLE') {
                     const titleText = node.textContent.trim();
                     if (titleText && window.t(titleText) !== titleText) {
                         node.textContent = window.t(titleText);
                     }
                 }
-
+                
                 node.childNodes.forEach(translateNode);
             }
         };
